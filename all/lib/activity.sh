5737969bd702da9599630028a77016e6f4be7f2f
Walter White <WalterWhite@walterjwhite.com>
2024-12-05 06:55:45 -0500

    default - merged

--- all/lib/activity.sh
+++ all/lib/activity.sh
@@ -1,16 +1,8 @@
-import git:git/project.directory.sh
 import git:install/time.sh
-conf git
 
-_GIT_WARN=1 _get_project_directory || {
-	[ $_ACTIVITY_PROJECT_DIR_ARG ] || _ACTIVITY_PROJECT_DIR_ARG=0
+_activity_decade_path() {
+	local date_path=$(date +%Y/%m.%B/%d/%H.%M.%S)
+	local decade=$(_time_decade)
 
-	if [ $# -gt $_ACTIVITY_PROJECT_DIR_ARG ]; then
-		_PROJECT_PATH=$_DATA_BASE_PATH/$1
-		shift
-	else
-		_error "Unable to determine project directory, insufficient arguments provided ($#) !> [$_ACTIVITY_PROJECT_DIR_ARG] "
-	fi
+	printf '%s/%s\n' "$decade" "$date_path"
 }
-
-_ACTIVITY_DIRECTORY=$_PROJECT_PATH/.activity
